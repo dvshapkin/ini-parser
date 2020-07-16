e346cc8442300ea9156fd644c8ea240a683753b7
dvshapkin <dvshapkin@mail.ru>
2020-07-16 20:02:35 +0700

    Config - v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Config<Value: FromStr> {
+    data: HashMap<String, HashMap<String, Box<Value>>>,
+}
+
+impl<Value: FromStr> Config<Value> {
+    pub fn new() -> Self {
+        Config {
+            data: HashMap::new(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
